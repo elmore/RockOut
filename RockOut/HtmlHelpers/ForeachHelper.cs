e99d97918967d59333ac2fc7a5502124b30f4db3
Tom Elmore <tom.elmore@easyroommate.com>
2015-10-30 17:00:40 +0000

    added helper to build html

---
+++ RockOut/HtmlHelpers/ForeachHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Web;
+
+namespace RockOut.HtmlHelpers
+{
+    public class RockOut
+    {
+        public static HtmlString Foreach(IEnumerable collection, Func<dynamic, string> template)
+        {
+            var sb = new StringBuilder();
+
+            foreach (dynamic el in collection)
+            {
+                sb.AppendLine(template(el));
+            }
+
+            string htmlString = sb.ToString();
+
+            return new HtmlString(htmlString);
+        }
+    }
+}
