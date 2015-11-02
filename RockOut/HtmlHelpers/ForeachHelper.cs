e99a8ceb1e7bc9ca30bb1b7e1dc873336f255641
Tom Elmore <tom.elmore@easyroommate.com>
2015-11-02 14:02:22 +0000

    changed li inner text to be referenced by string property name

--- RockOut/HtmlHelpers/ForeachHelper.cs
+++ RockOut/HtmlHelpers/ForeachHelper.cs
@@ -39,12 +39,12 @@ namespace RockOut.HtmlHelpers
 
     public class RepeatedHtmlElement
     {
-        private readonly string _text;
+        private readonly string _textFieldName;
         private readonly IDictionary<string, string> _attributes;
 
-        public RepeatedHtmlElement(string text, IDictionary<string, string> attributes)
+        public RepeatedHtmlElement(string textFieldName, IDictionary<string, string> attributes)
         {
-            _text = text;
+            _textFieldName = textFieldName;
             _attributes = attributes;
         }
 
@@ -54,7 +54,7 @@ namespace RockOut.HtmlHelpers
 
             allAtts.Add("data-bind", TemplateKoString());
 
-            string htmlString = Template(allAtts);
+            string htmlString = Template(model, allAtts);
 
             return new HtmlString(htmlString);
         }
@@ -63,25 +63,25 @@ namespace RockOut.HtmlHelpers
         {
             var allAtts = GetPropertyValues(model);
 
-            string htmlString = Template(allAtts);
+            string htmlString = Template(model, allAtts);
 
             return new HtmlString(htmlString);
         }
 
-        private string Template(IDictionary<string, string> allAtts)
+        private string Template(object model, IDictionary<string, string> allAtts)
         {
             var atts = allAtts.Select(attr => string.Format("{0}=\"{1}\"", attr.Key, attr.Value)).ToList();
 
             var attrSb = string.Join(" ", atts);
 
-            return string.Format("<li {0}>{1}</li>", attrSb, _text);
+            return string.Format("<li {0}>{1}</li>", attrSb, GetValue(model, _textFieldName));
         }
 
         private string TemplateKoString()
         {
             var koSb = _attributes.Select(attr => string.Format("{0} : {1}", attr.Key, attr.Value)).ToList();
 
-            return string.Format("text : Label, attr : {{ {0} }}", string.Join(",", koSb));
+            return string.Format("text : {0}, attr : {{ {1} }}", _textFieldName, string.Join(",", koSb));
         }
 
         private string GetValue(object model, string field)
